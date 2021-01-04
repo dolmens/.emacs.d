ccefeeeecf940574ae3f7d418199d4e3cba031a2
Sun I Ming <dolmens@gmail.com>
2021-01-04 11:44:07 +0800

    beautify orgmode

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -12,6 +12,17 @@
   :ensure
   org-plus-contrib)
 
+(use-package org-superstar
+  :hook ((org-mode . org-superstar-mode)))
+
+(defun my/org-mode-hook ()
+  (set-face-attribute 'org-level-1 nil :height 1.0 :box nil :background nil)
+  (set-face-attribute 'org-level-2 nil :height 1.0 :box nil :background nil)
+  (set-face-attribute 'org-level-3 nil :height 1.0 :box nil :background nil)
+  (set-face-attribute 'org-level-4 nil :height 1.0 :box nil :background nil))
+(add-hook 'org-mode-hook 'my/org-mode-hook)
+
+
 (provide 'init-org)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
